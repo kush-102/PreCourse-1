30bdcedd7069ce78c9c919d766c4a92e7f2d7e22
kush-102 <khmt1810@gmail.com>
2024-09-23 23:29:42 -0700

    Adding my changes

--- Exercise_3.py
+++ Exercise_3.py
@@ -2,8 +2,12 @@ class ListNode:
     """
     A node in a singly-linked list.
     """
+
     def __init__(self, data=None, next=None):
-    
+        self.data = data
+        self.next = next
+
+
 class SinglyLinkedList:
     def __init__(self):
         """
@@ -17,16 +21,51 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
-        
+        if self.head is None:
+            self.head = ListNode(data)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = ListNode(data)
+
     def find(self, key):
         """
         Search for the first element with `data` matching
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
-        
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            current = current.next
+        return None
+
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        previous = None
+        while current:
+            if current.data == key:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.head = current.next
+            previous = current
+            current = current.next
+        return False
+
+
+# time complexity
+# append: O(n)
+# find: O(n)
+# remove:O(n)
+
+# space complexity
+# append: O(1)
+# find: O(1)
+# remove:O(1)
