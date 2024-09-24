7e6c84108459bd522ca829b439378e9251c1e4b6
kush-102 <khmt1810@gmail.com>
2024-09-23 23:38:37 -0700

    Done Precourse1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,38 @@
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
-        
-        
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
-         
+    # Please read sample.java file before starting.
+    # Kindly include Time and Space complexity at top of each file
+    def __init__(self):
+
+        self.stack = []
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.isEmpty():
+            print("Stack is empty or underflow")
+        return self.stack.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+    def show(self):
+        return self.stack
+
 
 s = myStack()
-s.push('1')
-s.push('2')
+s.push("1")
+s.push("2")
 print(s.pop())
 print(s.show())
+
+# time complexity is push: O(1) (amortized), pop: O(1), peek:O(1), show:O(n) and space complexity is similar for all methods
--- Exercise_2.py
+++ Exercise_2.py
@@ -1,32 +1,46 @@
-
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
- 
+        self.data = data
+        self.next = None
+
+
 class Stack:
     def __init__(self):
-        
+        self.top = None
+
     def push(self, data):
-        
+        new_node = Node(data)
+        new_node.next = self.top
+        self.top = new_node
+
     def pop(self):
-        
+        if self.top == None:
+            return None
+        else:
+            curr_top = self.top
+            self.top = self.top.next
+            return curr_top.data
+
+
 a_stack = Stack()
 while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print("push <value>")
+    print("pop")
+    print("quit")
+    do = input("What would you like to do? ").split()
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     operation = do[0].strip().lower()
-    if operation == 'push':
+    if operation == "push":
         a_stack.push(int(do[1]))
-    elif operation == 'pop':
+    elif operation == "pop":
         popped = a_stack.pop()
         if popped is None:
-            print('Stack is empty.')
+            print("Stack is empty.")
         else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
+            print("Popped value: ", int(popped))
+    elif operation == "quit":
         break
+
+# time complexity is O(1) for push and pop
+# space complexity is O(1) for pop and push
